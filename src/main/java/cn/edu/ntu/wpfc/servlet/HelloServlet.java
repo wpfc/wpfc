500085dd776605e7d9ee4ce3dc273a4bea18d468
wpfc <2570721221@qq.com>
2017-06-27 20:58:12 +0800

    commit src to git

---
+++ src/main/java/cn/edu/ntu/wpfc/servlet/HelloServlet.java
@@ -0,0 +1,35 @@
+package cn.edu.ntu.wpfc.servlet;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String currentTime = dateFormat.format(new Date());
+		req.setAttribute("currentTime", currentTime);
+		req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+	
+}
